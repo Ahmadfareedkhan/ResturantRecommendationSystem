85401d4769cddf500f3b98c7dcd1c66a78cf147e
Ahmadfareedkhan <ahmadkhanfareed388@gmail.com>
2024-06-19 23:31:52 +0500

    chnages in flask app to integratwe with web

--- flask_app.py
+++ flask_app.py
@@ -1,38 +1,53 @@
-from flask import Flask, render_template, jsonify
+from flask import Flask, render_template, request
+import pandas as pd
 import os
 
-TEMPLATE_DIR = os.path.abspath(
-    r"C:\Users\ahmad\.vscode\ResturantRecommendationSystem\templates"
-)
-STATIC_DIR = os.path.abspath(
-    r"C:\Users\ahmad\.vscode\ResturantRecommendationSystem\static"
-)
+TEMPLATE_DIR = os.path.abspath(r"C:\Users\ahmad\.vscode\ResturantRecommendationSystem\templates")
+STATIC_DIR = os.path.abspath(r"C:\Users\ahmad\.vscode\ResturantRecommendationSystem\static")
 
 app = Flask(__name__, template_folder=TEMPLATE_DIR, static_folder=STATIC_DIR)
 
-# Dummy data - you should replace this with your actual restaurant recommendation logic
-restaurants = [
-    {"name": "Chez Panisse", "location": "Berkeley, CA", "cuisine": "French"},
-    {"name": "Shizen Vegan Sushi", "location": "San Francisco, CA", "cuisine": "Japanese"}
-]
+# Load the restaurant data from Excel
+restaurant_data = pd.read_excel(r'C:\Users\ahmad\.vscode\ResturantRecommendationSystem\Resturant_data_1000.xlsx')  # Update the path to your Excel file
 
-@app.route('/')
+@app.route('/', methods=['GET', 'POST'])
 def index():
-    return render_template('index.html', restaurants=restaurants)
+    cities = restaurant_data['City'].unique()
+    cuisines = restaurant_data['Cuisine'].unique()
+    locations = restaurant_data['Location'].unique()
+    filtered_restaurants = None
+    recommendations = None
 
-@app.route('/api/restaurants')
-def api_restaurants():
-    return jsonify(restaurants)
+    if request.method == 'POST':
+        city = request.form.get('city')
+        cuisine_type = request.form.get('cuisine_type')
+        location = request.form.get('location')
+        min_rating = request.form.get('min_rating')
 
+        filtered_restaurants = restaurant_data[(restaurant_data['City'] == city) &
+                                               (restaurant_data['Cuisine'] == cuisine_type) &
+                                               (restaurant_data['Location'] == location)]
 
-@app.route('/login')
-def login():
-    return render_template('login.html')
+        if min_rating:
+            min_rating = float(min_rating)
+            if 'Rating' in filtered_restaurants.columns:
+                filtered_restaurants = filtered_restaurants[filtered_restaurants['Rating'].astype(float) >= min_rating]
 
-@app.route('/contact')
-def contact():
-    return render_template('contactus.html')
+        if not filtered_restaurants.empty:
+            recommended_restaurant = filtered_restaurants.iloc[0]['Restaurant Name']
+            recommendations = get_recommendations(recommended_restaurant)
 
+    return render_template('index.html', 
+                           cities=cities, 
+                           cuisines=cuisines, 
+                           locations=locations, 
+                           filtered_restaurants=filtered_restaurants, 
+                           recommendations=recommendations)
+
+def get_recommendations(restaurant_name):
+    restaurant_index = restaurant_data[restaurant_data['Restaurant Name'] == restaurant_name].index[0]
+    recommendations = restaurant_data.loc[restaurant_index + 1:restaurant_index + 6, ['Restaurant Name', 'Cuisine', 'Location', 'Rating']]
+    return recommendations
 
 if __name__ == '__main__':
     app.run(debug=True)
